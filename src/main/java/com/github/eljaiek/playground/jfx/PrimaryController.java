c5de2fbb16d81fc0de32359f775ba1f428dd76ad
ehayik <eduardo.eljaiek@gmail.com>
2023-01-07 23:04:18 +0200

    test properties injection

--- src/main/java/com/github/eljaiek/playground/jfx/PrimaryController.java
+++ src/main/java/com/github/eljaiek/playground/jfx/PrimaryController.java
@@ -1,17 +1,26 @@
 package com.github.eljaiek.playground.jfx;
 
 import javafx.fxml.FXML;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
+@Slf4j
 @Controller
-@RequiredArgsConstructor
 public class PrimaryController {
+
+    private final String appMessage;
 
     private final StageRouter router;
 
+    public PrimaryController(@Value("${app.message}") String appMessage, StageRouter router) {
+        this.appMessage = appMessage;
+        this.router = router;
+    }
+
     @FXML
     private void switchToSecondary() {
+        log.info(appMessage);
         router.navigateByUrl("/secondary");
     }
 }
